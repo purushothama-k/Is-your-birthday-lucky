de3cd22a242276eebf877ef31e591d5a0222f5b4
purushothama-k <98885162+purushothama-k@users.noreply.github.com>
2022-07-31 14:51:43 +0530

    Add files via upload

---
+++ Is your bithday lucky (mark 11)/js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const dateOfBirthEl = document.querySelector('#date-of-birth');
+const luckyNumberEl = document.querySelector('#lucky-number');
+const luckBtn = document.querySelector('.btn');
+
+const checkDobIsLucky = function () {
+  const birthdate = dateOfBirthEl.value;
+  const sum = calculateDobSum(birthdate);
+
+  compareData(sum, luckyNumberEl.value);
+};
+
+const calculateDobSum = function (birthdate) {
+  birthdate = birthdate.replaceAll('-', '');
+  let totalSum = 0;
+  for (let i = 0; i < birthdate.length; i++) {
+    totalSum = totalSum + Number(birthdate[i]);
+  }
+  return totalSum;
+};
+
+const compareData = function (sum, luckyNumberEl) {
+  if (sum % luckyNumberEl === 0) {
+    document.querySelector('.output-message').textContent =
+      '🥳 Hurray! your birthday is lucky 🥳';
+  } else {
+    document.querySelector('.output-message').textContent =
+      ' 😯 Your birthday is not lucky 😯';
+  }
+};
+
+luckBtn.addEventListener('click', checkDobIsLucky);
